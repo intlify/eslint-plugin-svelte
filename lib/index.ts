b297af964f1a7ff05d235ed1ad283867576cc579
Iniubong Obonguko <iniubongobonguko2018@gmail.com>
2024-06-30 13:19:21 +0100

    chore: module exports

--- lib/index.ts
+++ lib/index.ts
@@ -2,12 +2,23 @@
  * @fileoverview ESLint plugin for internationalization with Svelte
  * @author Yosuke Ota
  */
-import configs from './configs'
+import legacyBase from './configs/base'
+import legacyRecommended from './configs/recommended'
+import flatBase from './configs/flat/base'
+import flatRecommended from './configs/flat/recommended'
 import rules from './rules'
 import * as meta from './meta'
 
 export = {
   meta,
-  configs,
+  // eslintrc configs
+  configs: {
+    base: legacyBase,
+    recommended: legacyRecommended,
+
+    // flat configs
+    'flat/base': flatBase,
+    'flat/recommended': flatRecommended
+  },
   rules
 }
