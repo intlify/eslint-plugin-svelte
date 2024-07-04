4f531e080bed495479a6a5643b3ae88b20676138
Iniubong Obonguko <iniubongobonguko2018@gmail.com>
2024-07-04 15:13:54 +0100

    feat: setup parser for svelte file

--- tests-integrations/flat-config/eslint.config.js
+++ tests-integrations/flat-config/eslint.config.js
@@ -1,9 +1,13 @@
 import intlifySvelte from '@intlify/eslint-plugin-svelte'
+import svelteParser from 'svelte-eslint-parser'
 
 export default [
   ...intlifySvelte.configs['flat/recommended'],
   {
-    files: ['**/*.svelte'],
+    files: ['**/*.svelte', '*.svelte'],
+    languageOptions: {
+      parser: svelteParser
+    },
     rules: {
       '@intlify/svelte/no-raw-text': 'error'
     }
